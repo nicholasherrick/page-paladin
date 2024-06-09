29cd1bc5e4e5e65062413b7dc4f4be46fadc91ba
Nicholas Herrick <nicholas.herrick95@gmail.com>
2024-06-09 00:12:55 -0600

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import './App.css';
-import { useEffect } from 'react';
-import { themeChange } from 'theme-change';
-import Navbar from './components/ThemeController';
+import './App.css'
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
+import Navbar from './components/ThemeController'
 
 function App() {
   useEffect(() => {
     themeChange(false)
-     // 👆 false parameter is required for react project
+    // 👆 false parameter is required for react project
   }, [])
 
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
       <h1>Welcome to Page Paladin!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
